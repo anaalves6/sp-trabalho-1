3f637c6db2f411861429987e6a2aac1fc0a17336
Lara Queiroga <lara.queiroga06@gmail.com>
2026-04-01 11:08:31 +0100

    main atualizado

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from aes import run_aes, plot_aes, plot_first_15_runs
+from aes import run_aes, plot_aes, plot_same_vs_diff_files
 from rsa2048 import run_rsa, plot_rsa
 from sha256 import run_sha, plot_sha
 
 run_aes()
 plot_aes()
-plot_first_15_runs()
+plot_same_vs_diff_files()
 
 run_rsa()
 plot_rsa()
